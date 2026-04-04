027966cd26b08e96b261c27daaae2eb8ab208a7a
jal1972 <jadenle094@gmail.com>
2026-04-03 19:11:15 -0500

    Update PostController.java from old Git

--- src/main/java/PostController.java
+++ src/main/java/PostController.java
@@ -1,2 +1,21 @@
-public class PostController {
+public class PostController
+{
+    private Post post = new Post();
+    public String currUser = Main.currUser;
+    private boolean check;
+
+    public String newPost(String title, double price, String description)
+    {
+        User user = DBManager.getUser(currUser);
+        if(user.hasPost(title))
+            return "This post already exists";
+        
+        check = post.createPost(title, price, description);
+        
+        if(check){
+            DBManager.storePost(currUser, post);
+            return "A new post has been created.\n";
+        }else
+            return "A field has an incorrect input, please try again.\n";
+    }
 }
