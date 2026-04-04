d773042ed61ac1b526d7ce4eee7b33e369c716ee
jal1972 <jadenle094@gmail.com>
2026-04-03 19:10:46 -0500

    Update Post.java from old Git

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,20 +1,48 @@
+import java.lang.Math;
+import java.util.String;
+
 public class Post
 {
-    private int price;
-    private String name;
+    private double price;
+    private String title;
     private String description;
-
-    public Post(int price, String name, String description)
+    
+    public Post()
     {
+        
+    }
+    
+    public boolean createPost(String title, double price, String description)
+    {
+        if((price<1&&price>10000)||(fmod(price, 0.01))!=0||(title.length()>40&&title.length<1)||description.length()>300)
+        {
+            return false;
+        }
         this.price = price;
-        this.name = name;
+        this.title = title;
         this.description = description;
+        return true;
+    }
+
+    public String getName()
+    {
+        return title;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String getDescription()
+    {
+        return description;
     }
 
     @Override
     public String toString()
     {
-        return "Product: " + "\n" + "Price: " + price + "$\n"
-                + "Description: " + description + "\n";
+        return "Product: " + "\n" + "Price: " + price + "$\n" 
+            + "Description: " + description + "\n";
     }
 }
